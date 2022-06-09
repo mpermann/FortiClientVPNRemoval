b6e0ba849291da8144810860517baa790974b78e
Michael Permann <mike.permann@gmail.com>
2022-06-09 15:03:59 -0500

    Update FortiClientVPNRemoval.sh
    Modifications to add using uninstall_helper method mentioned by seb.fisher on MacAdmins Slack channel.

--- FortiClientVPNRemoval.sh
+++ FortiClientVPNRemoval.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Name: FortiClientVPNRemoval.sh
-# Version: 1.0.1
+# Version: 1.0.2
 # Created: 05-29-2022 by Michael Permann
-# Modified: 06-06-2022
+# Modified: 06-09-2022
 # Purpose: Removes the FortiClient VPN software.
 
 CURRENT_USER=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
@@ -11,31 +11,23 @@ USER_ID=$(/usr/bin/id -u "$CURRENT_USER")
 LOGO="/Library/Application Support/HeartlandAEA11/Images/HeartlandLogo@512px.png"
 JAMF_HELPER="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
 JAMF_BINARY=$(which jamf)
-TITLE="Uninstall Application"
-DESCRIPTION="The FortiClient VPN will be removed. 
+TITLE="Restart Needed"
+DESCRIPTION="Your computer needs restarted to complete the removal.
 
-1. Click the \"Uninstall\" button on the FortiClient Uninstaller dialog. 
-2. Enter your computer password and click the \"Install Helper\" button.
-3. Click the \"Done\" button to close FortiClient Uninstaller.
+Click the \"OK\" button to restart your computer. Your computer will restart automatically 1 minute after clicking the \"OK\" button."
 
-Then click the \"OK\" button to dismiss this dialog."
-BUTTON1="OK"
+BUTTON="OK"
 DEFAULT_BUTTON="1"
-TITLE1="Restart Needed"
-DESCRIPTION1="Your computer needs restarted to complete the removal.
+VPN_REMOVAL_APP="/Applications/FortiClientUninstaller.app/Contents/Library/LaunchServices/com.fortinet.forticlient.uninstall_helper"
 
-Click the \"OK\" button to restart your computer. Your computer will restart automatically after 1 minute."
-VPN_APP="/Applications/FortiClient.app/Contents/MacOS/FortiClient"
-
-if [ -f "$VPN_APP" ]
+if [ -f "$VPN_REMOVAL_APP" ]
 then
     echo "FortiClient software present"
-    echo "Launching FortiClientUninstaller.app"
-    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "/usr/bin/open" "/Applications/FortiClientUninstaller.app"
-    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "$TITLE" -description "$DESCRIPTION" -icon "$LOGO" -button1 "$BUTTON1" -defaultButton "$DEFAULT_BUTTON"
+    echo "Running com.fortinet.forticlient.uninstall_helper removal app to remove software" # Credit to seb.fisher on MacAdmins Slack #fortinet channel for finding this removal option
+    /Applications/FortiClientUninstaller.app/Contents/Library/LaunchServices/com.fortinet.forticlient.uninstall_helper
     echo "Updating inventory"
     "$JAMF_BINARY" recon
-    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "$TITLE1" -description "$DESCRIPTION1" -icon "$LOGO" -button1 "$BUTTON1" -defaultButton "$DEFAULT_BUTTON"
+    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "$TITLE" -description "$DESCRIPTION" -icon "$LOGO" -button1 "$BUTTON" -defaultButton "$DEFAULT_BUTTON"
     /sbin/shutdown -r +1 &
 else
     echo "FortiClient software NOT present"
